2b900984981389f10aa70339394088ff6cb367aa
Yuxiang Zhu <vfreex@gmail.com>
2022-11-11 15:51:42 +0800

    Init commit

---
+++ pages/new-content.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head"
+
+export default function NewContent() {
+    return (
+        <>
+            <Head>
+                <title>Request New Content</title>
+            </Head>
+            <div>Request a new image or RPM to be managed by ART and released with OCP.</div>
+        </>
+    )
+}
