70d5a4600bdabffa5ec5dbf8f2432ed8c0db1e71
Ashwin Das <ashwindasr@gmail.com>
2023-04-19 19:31:25 -0400

    remove server route for art-build-dev
    Now that we're migrating to the new cluster, we don't need to have two configurations for server in prod and dev

--- components/api_calls/release_calls.js
+++ components/api_calls/release_calls.js
@@ -4,9 +4,6 @@ if (process.env.NEXT_PUBLIC_RUN_ENV === "dev") {
     server_endpoint = "http://localhost:8080/"
 } else {
     server_endpoint = process.env.NEXT_PUBLIC_ART_DASH_SERVER_ROUTE + "/"
-
-    // OPENSHIFT_BUILD_NAMESPACE env variable is obtained inside pod during its run
-    server_endpoint = server_endpoint.replace(/\{0\}/g, process.env.NEXT_PUBLIC_OPENSHIFT_BUILD_NAMESPACE);
 }
 
 
