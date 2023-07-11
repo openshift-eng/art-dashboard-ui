0efe9a33819ebbcbbbab234f299899e3d3c37913
MartinPl0 <mathas000@gmail.com>
2023-07-11 19:17:02 +0200

    Consolidated the API calls into a single function
    Previously, we had multiple functions scattered across different files for making API requests to the server. Now, we have a centralized function in a separate file that allows us to make any type of request (GET/POST) by specifying the URL.
    
    This refactoring will help streamline our codebase and make it easier to manage and maintain API-related logic. By having a single function, we can avoid code duplication and ensure consistency in handling API requests throughout the application.
    
    Additionally, this change lays the foundation for future enhancements and improvements to our API handling.

--- components/api_calls/build_calls.js
+++ components/api_calls/build_calls.js
@@ -1,41 +1,26 @@
-let server_endpoint = null
-
-if (process.env.NEXT_PUBLIC_RUN_ENV === "dev") {
-    server_endpoint = "http://localhost:8080"
-} else {
-    server_endpoint = process.env.NEXT_PUBLIC_ART_DASH_SERVER_ROUTE + "/"
-}
+import { makeApiCall } from './api_calls';
 
 export async function getBuilds(searchParams) {
+    let params = {};
 
-    let query = ""
     for (const key in searchParams) {
-        let value = searchParams[key]
+        let value = searchParams[key];
 
         if ((value[0] === "\"" && value[value.length - 1] === "\"") || (value[0] === "'" && value[value.length - 1] === "'")) {
-            value = value.substring(1, value.length - 1)
-            query += `${key}=${value}&`
-            continue
+            value = value.substring(1, value.length - 1);
         }
 
         if (key !== "time_iso" && key !== "page" && key !== "brew_task_state")
-            query += `${key}__icontains=${value}&`
+            params[`${key}__icontains`] = value;
         else
-            query += `${key}=${value}&`
+            params[key] = value;
     }
 
-    if (!(query === "")) {
-        query = "?" + query.substring(0, query.length - 1)  // Remove additional & at the end and add ? to the beginning
-    }
+    const headers = {
+        'Accept': 'application/json',
+        "Content-Type": "application/json"
+    };
 
-    const response = await fetch(server_endpoint + process.env.NEXT_PUBLIC_BUILD_ENDPOINT + query, {
-        method: "GET",
-        headers: {
-            'Accept': 'application/json',
-            "Content-Type": "application/json"
-        }
-    });
-
-    return await response.json();
-
+    return makeApiCall(process.env.NEXT_PUBLIC_BUILD_ENDPOINT, 'GET', headers, params)
+        .catch(error => console.error('Error:', error));
 }
--- components/api_calls/release_calls.js
+++ components/api_calls/release_calls.js
@@ -1,90 +1,36 @@
-let server_endpoint = null
+import { makeApiCall } from './api_calls';
 
-if (process.env.NEXT_PUBLIC_RUN_ENV === "dev") {
-    server_endpoint = "http://localhost:8080/"
-} else {
-    server_endpoint = process.env.NEXT_PUBLIC_ART_DASH_SERVER_ROUTE + "/"
+const headers = {
+    'Accept': 'application/json',
+    "Content-Type": "application/json"
+};
+
+export function getReleaseBranchesFromOcpBuildData() {
+    const url = `${process.env.NEXT_PUBLIC_API_ENDPOINT}${process.env.NEXT_PUBLIC_OCP_BUILD_DATA_OPENSHIFT_BRANCH_ENDPOINT}`;
+    return makeApiCall(url, 'GET', headers);
 }
 
-
-export async function getReleaseBranchesFromOcpBuildData() {
-
-    const response = await fetch(server_endpoint + process.env.NEXT_PUBLIC_API_ENDPOINT + process.env.NEXT_PUBLIC_OCP_BUILD_DATA_OPENSHIFT_BRANCH_ENDPOINT, {
-        method: "GET",
-        headers: {
-            'Accept': 'application/json',
-            "Content-Type": "application/json",
-        }
-    });
-
-    return await response.json();
+export function remaining_git_requests() {
+    const url = `${process.env.NEXT_PUBLIC_GIT_HUB_RATE_LIMIT_ENDPOINT}`;
+    return makeApiCall(url, 'GET', headers);
 }
 
-
-export async function remaining_git_requests() {
-
-    const response = await fetch(server_endpoint + process.env.NEXT_PUBLIC_GIT_HUB_RATE_LIMIT_ENDPOINT, {
-        method: "GET",
-        headers: {
-            'Accept': 'application/json',
-            "Content-Type": "application/json",
-        }
-    });
-
-    return await response.json();
-
+export function advisory_ids_for_branch(branch_name) {
+    const url = `${process.env.NEXT_PUBLIC_API_ENDPOINT}${process.env.NEXT_PUBLIC_CURRENT_ADVISORY_IDS_FOR_A_BRANCH_OCP_BUILD_DATA}${branch_name}`;
+    return makeApiCall(url, 'GET', headers);
 }
 
-export async function advisory_ids_for_branch(branch_name) {
-
-    const response = await fetch(server_endpoint + process.env.NEXT_PUBLIC_API_ENDPOINT + process.env.NEXT_PUBLIC_CURRENT_ADVISORY_IDS_FOR_A_BRANCH_OCP_BUILD_DATA + branch_name, {
-        method: "GET",
-        headers: {
-            'Accept': 'application/json',
-            "Content-Type": "application/json",
-        }
-    });
-
-    return await response.json();
-
+export function advisory_details_for_advisory_id(advisory_id) {
+    const url = `${process.env.NEXT_PUBLIC_API_ENDPOINT}${process.env.NEXT_PUBLIC_ADVISORY_DETAILS_FOR_AN_ADVISORY_ID}${advisory_id}`;
+    return makeApiCall(url, 'GET', headers);
 }
 
-export async function advisory_details_for_advisory_id(advisory_id) {
-
-    const response = await fetch(server_endpoint + process.env.NEXT_PUBLIC_API_ENDPOINT + process.env.NEXT_PUBLIC_ADVISORY_DETAILS_FOR_AN_ADVISORY_ID + advisory_id, {
-        method: "GET",
-        headers: {
-            'Accept': 'application/json',
-            "Content-Type": "application/json",
-        }
-    });
-
-    return await response.json();
-
+export function user_details_for_user_id(user_id) {
+    const url = `${process.env.NEXT_PUBLIC_USER_DETAILS_FOR_A_USER_ID}${user_id}`;
+    return makeApiCall(url, 'GET', headers);
 }
 
-export async function user_details_for_user_id(user_id) {
-
-    const response = await fetch(server_endpoint + process.env.NEXT_PUBLIC_USER_DETAILS_FOR_A_USER_ID + user_id, {
-        method: "GET",
-        headers: {
-            'Accept': 'application/json',
-            "Content-Type": "application/json",
-        }
-    });
-
-    return await response.json();
-}
-
-
-export async function gaVersion() {
-    const response = await fetch(server_endpoint + process.env.NEXT_PUBLIC_API_ENDPOINT + "/ga-version", {
-        method: "GET",
-        headers: {
-            'Accept': 'application/json',
-            "Content-Type": "application/json",
-        }
-    });
-
-    return await response.json();
+export function gaVersion() {
+    const url = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/ga-version`;
+    return makeApiCall(url, 'GET', headers);
 }
