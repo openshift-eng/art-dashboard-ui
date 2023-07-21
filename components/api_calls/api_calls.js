433b9fb933833af7396f8fcda0ab8e75dcaf5adc
MartinPl0 <mathas000@gmail.com>
2023-07-21 16:32:57 +0200

    Implement login functionality and private page
    Added Login.js component, withAuth.js component, and related pages and styles. Also updated api_calls.js and index.js files to correlate these changes

--- components/api_calls/api_calls.js
+++ components/api_calls/api_calls.js
@@ -8,46 +8,52 @@ if (process.env.NEXT_PUBLIC_RUN_ENV === "dev") {
     server_endpoint = process.env.NEXT_PUBLIC_ART_DASH_SERVER_ROUTE;
 }
 
-export function makeApiCall(urlPath, method, headers = {}, params = {}) {
+function getCsrfToken() {
+    if (typeof document === 'undefined' || !document.cookie) {
+        return null;
+    }
+    const token = document.cookie.split(';')
+        .map(c => c.trim())
+        .filter(c => c.startsWith('csrftoken='));
+
+    if (token.length === 0) {
+        return null;
+    }
+    return decodeURIComponent(token[0].split('=')[1]);
+}
+
+export function makeApiCall(urlPath, method, data = {}, headers = {}, params = {}, req = null) {
     if (!server_endpoint || !urlPath) {
-        console.error("Server endpoint or URL path is not defined");
         return;
     }
 
-    // Ensure urlPath always starts with a '/'
     if (!urlPath.startsWith('/')) {
         urlPath = '/' + urlPath;
     }
 
+    if (req) {
+        headers['cookie'] = req.headers.cookie;
+    }
+
     const url = `${server_endpoint}${urlPath}`;
     
+    const csrfToken = getCsrfToken();
+    if (csrfToken) {
+        headers['X-CSRFToken'] = csrfToken;
+    }
+
     return axios({
         method: method,
         url: url,
         headers: headers,
-        params: params
+        params: params,
+        data: data,
+        withCredentials: true
     })
     .then(response => {
-        console.log('Success:', response);
         return response.data;
     })
     .catch(error => {
-        console.error('Error:', error);
-        if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.error('Response data:', error.response.data);
-            console.error('Response status:', error.response.status);
-            console.error('Response headers:', error.response.headers);
-        } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-            // http.ClientRequest in node.js
-            console.error('Request:', error.request);
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            console.error('Message:', error.message);
-        }
-        console.error('Config:', error.config);
+        return { detail: 'Request failed' };
     });
 }
