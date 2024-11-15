1e5dcd2f04f873b6328bc9e8e9fe4f3869e07f24
D. Paolella <dpaolell@redhat.com>
2024-11-15 17:56:42 +0100

    Add no results message

--- static/script.js
+++ static/script.js
@@ -13,7 +13,7 @@ function hideLoading() {
 }
 
 document.getElementById("searchButton").addEventListener("click", function () {
-    showLoading()
+    showLoading();
     const form = document.getElementById("searchForm");
     const formData = new FormData(form);
 
@@ -27,30 +27,35 @@ document.getElementById("searchButton").addEventListener("click", function () {
             tableBody.innerHTML = ""; // Clear existing rows
 
             // Populate table with new results
-            data.forEach((result) => {
-                const row = document.createElement("tr");
+            // If no results are returned, show the "No Results" message
+            if (data.length === 0) {
+                noResultsMessage.style.display = "block";
+            } else {
+                data.forEach((result) => {
+                    const row = document.createElement("tr");
 
-                // Determine the Outcome display value
-                let outcomeDisplay;
-                if (result.Outcome.toLowerCase() === "success") {
-                    outcomeDisplay = "✅";
-                } else if (result.Outcome.toLowerCase() === "failure") {
-                    outcomeDisplay = "❌";
-                } else {
-                    outcomeDisplay = result.Outcome; // Fallback for other cases
-                }
+                    // Determine the Outcome display value
+                    let outcomeDisplay;
+                    if (result.Outcome.toLowerCase() === "success") {
+                        outcomeDisplay = "✅";
+                    } else if (result.Outcome.toLowerCase() === "failure") {
+                        outcomeDisplay = "❌";
+                    } else {
+                        outcomeDisplay = result.Outcome; // Fallback for other cases
+                    }
 
-                // Create the row
-                row.innerHTML = `
-                    <td class="nvr-td">${result.NVR}</td>
-                    <td>${outcomeDisplay}</td>
-                    <td>${result["Assembly"]}</td>
-                    <td>${result["Group"]}</td>
-                `;
-                tableBody.appendChild(row);
-            });
+                    // Create the row
+                    row.innerHTML = `
+                        <td class="nvr-td">${result.NVR}</td>
+                        <td>${outcomeDisplay}</td>
+                        <td>${result["Assembly"]}</td>
+                        <td>${result["Group"]}</td>
+                    `;
+                    tableBody.appendChild(row);
+                });
+            }
 
-            hideLoading()
+            hideLoading();
         })
         .catch((error) => {
             console.error("Error:", error);
